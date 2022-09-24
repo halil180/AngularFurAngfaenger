ee6ea7612de82274bb60eb17e899ea8691b5a969
halil180 <halilkom180@.com>
2022-09-24 16:45:46 +0200

    input & output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'secondAngular';
+  postTexts= [
+    "Haallo, mein Name ist Frederick! Ich bin hier, um neue Freunde zu treffen",
+    "Hallo zusammen, Freut mich, dass ihr hier seid",
+    "Hallo mein Name ist Susanne. Ich bin 2 Jahre alt",
+    "Ich esse gerne Kekse",
+  ]
 
-
+  images= [
+    "assets/img/hamster/1.jpeg",
+    "assets/img/hamster/2.jpeg",
+    "assets/img/hamster/3.jpeg",
+    "assets/img/hamster/4.jpeg",
+  ]
   buttonClicked(){
     alert("Hallo, wie geht es dir?")
   }
